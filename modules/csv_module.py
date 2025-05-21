c84dee7294aa81083dac6d177c47d4fe1242f1b1
Ace <edisonford2006@gmail.com>
2025-05-21 23:10:22 +0100

    csv adaptations

--- modules/csv_module.py
+++ modules/csv_module.py
@@ -2,10 +2,17 @@ import csv
 
 class csv_manager:
     def __init__(self):
-        file_path = "assets/item_menu.csv"
+        self.menu_file_path = "assets/item_menu.csv"
+        self.customer_file_path = "assets/customers.csv"
     
     def write_to_menu():
         pass
 
     def read_from_menu():
+        pass
+
+    def write_to_customers():
+        pass
+
+    def read_from_customers():
         pass
