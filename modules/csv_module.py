2b2ae637e1ac2a27d2a9f008146c999214e64757
Ace <edisonford2006@gmail.com>
2025-05-21 23:52:50 +0100

    csv read/write menu

--- modules/csv_module.py
+++ modules/csv_module.py
@@ -5,14 +5,22 @@ class csv_manager:
         self.menu_file_path = "assets/item_menu.csv"
         self.customer_file_path = "assets/customers.csv"
     
-    def write_to_menu():
+    def write_to_menu(self, data:list[list]):
+        with open(self.menu_file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
+
+    def read_from_menu(self) -> list[list]:
+        menu_items = []
+        with open(self.menu_file_path, mode ='r')as file:
+          csvFile = csv.reader(file)
+          for lines in csvFile:
+                menu_items.append(list(lines))
+        
+        return menu_items
+
+    def write_to_customers(self):
         pass
 
-    def read_from_menu():
-        pass
-
-    def write_to_customers():
-        pass
-
-    def read_from_customers():
+    def read_from_customers(self):
         pass
