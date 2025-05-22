d44df4d9b34c9a5451418057eac7ca3ceccdb87d
Ace <edisonford2006@gmail.com>
2025-05-22 13:08:27 +0100

    csv fix

--- modules/csv_module.py
+++ modules/csv_module.py
@@ -19,8 +19,16 @@ class csv_manager:
         
         return menu_items
 
-    def write_to_customers(self):
-        pass
+    def write_to_customers(self, data:list[list]):
+        with open(self.customer_file_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
 
-    def read_from_customers(self):
-        pass
+    def read_from_customers(self) -> list[list]:
+        menu_items = []
+        with open(self.customer_file_path, mode ='r')as file:
+          csvFile = csv.reader(file)
+          for lines in csvFile:
+                menu_items.append(list(lines))
+        
+        return menu_items
